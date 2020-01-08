ce53e0b5f3ab39ad69415dda7f2739f8e32e6aa0
Ilya Portnov <portnov@bk.ru>
2020-01-08 19:21:38 +0500

    Minimally functional node.

--- nodes/surface/minimal_surface.py
+++ nodes/surface/minimal_surface.py
@@ -35,12 +35,54 @@ class SvExMinimalSurfaceNode(bpy.types.Node, SverchCustomTreeNode):
             default = 'multiquadric',
             update = updateNode)
 
+    grid_points : IntProperty(
+            name = "Points",
+            default = 25,
+            min = 3,
+            update = updateNode)
+
+    epsilon : FloatProperty(
+            name = "Epsilon",
+            default = 1.0,
+            min = 0.0,
+            update = updateNode)
+    
+    smooth : FloatProperty(
+            name = "Smooth",
+            default = 0.0,
+            min = 0.0,
+            update = updateNode)
+
     def sv_init(self, context):
         self.inputs.new('SvVerticesSocket', "Vertices")
+        self.inputs.new('SvStringsSocket', "GridPoints").prop_name = 'grid_points'
+        self.inputs.new('SvStringsSocket', "Epsilon").prop_name = 'epsilon'
+        self.inputs.new('SvStringsSocket', "Smooth").prop_name = 'smooth'
         self.outputs.new('SvVerticesSocket', "Vertices")
+        self.outputs.new('SvStringsSocket', "Edges")
+        self.outputs.new('SvStringsSocket', "Faces")
 
     def draw_buttons(self, context, layout):
         layout.prop(self, "function")
+
+    def make_edges(self, n_points):
+        edges = []
+        for row in range(n_points):
+            e_row = [(i + n_points * row, (i+1) + n_points * row) for i in range(n_points-1)]
+            edges.extend(e_row)
+            if row < n_points - 1:
+                e_col = [(i + n_points * row, i + n_points * (row+1)) for i in range(n_points)]
+                edges.extend(e_col)
+        return edges
+
+    def make_faces(self, n_points):
+        faces = []
+        for row in range(n_points - 1):
+            for col in range(n_points - 1):
+                i = row + col * n_points
+                face = (i, i+n_points, i+n_points+1, i+1)
+                faces.append(face)
+        return faces
 
     def process(self):
 
@@ -51,29 +93,44 @@ class SvExMinimalSurfaceNode(bpy.types.Node, SverchCustomTreeNode):
             return
 
         vertices_s = self.inputs['Vertices'].sv_get()
-
-        GRID_POINTS = 25
+        points_s = self.inputs['GridPoints'].sv_get()
+        epsilon_s = self.inputs['Epsilon'].sv_get()
+        smooth_s = self.inputs['Smooth'].sv_get()
 
         verts_out = []
-        for vertices in vertices_s:
+        edges_out = []
+        faces_out = []
+        for vertices, grid_points, epsilon, smooth in zip_long_repeat(vertices_s, points_s, epsilon_s, smooth_s):
+            if isinstance(epsilon, (list, int)):
+                epsilon = epsilon[0]
+            if isinstance(smooth, (list, int)):
+                smooth = smooth[0]
+            if isinstance(grid_points, (list, int)):
+                grid_points = grid_points[0]
+
             XYZ = np.array(vertices)
             x_min = XYZ[:,0].min()
             x_max = XYZ[:,0].max()
             y_min = XYZ[:,1].min()
             y_max = XYZ[:,1].max()
-            xi = np.linspace(x_min, x_max, GRID_POINTS)
-            yi = np.linspace(y_min, y_max, GRID_POINTS)
+            xi = np.linspace(x_min, x_max, grid_points)
+            yi = np.linspace(y_min, y_max, grid_points)
             XI, YI = np.meshgrid(xi, yi)
-            smooth = 0.0
-            epsilon = 1.0
 
             rbf = Rbf(XYZ[:,0],XYZ[:,1],XYZ[:,2],function=self.function,smooth=smooth,epsilon=epsilon)
             ZI = rbf(XI,YI)
 
             new_verts = np.dstack((XI,YI,ZI)).tolist()
+            new_verts = sum(new_verts, [])
+            new_edges = self.make_edges(grid_points)
+            new_faces = self.make_faces(grid_points)
             verts_out.append(new_verts)
+            edges_out.append(new_edges)
+            faces_out.append(new_faces)
 
         self.outputs['Vertices'].sv_set(verts_out)
+        self.outputs['Edges'].sv_set(edges_out)
+        self.outputs['Faces'].sv_set(faces_out)
 
 def register():
     bpy.utils.register_class(SvExMinimalSurfaceNode)
