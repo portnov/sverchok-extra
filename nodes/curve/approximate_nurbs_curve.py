61716650733e26c88a6a8d8b9f2eab61eb2bc91e
Ilya Portnov <portnov@bk.ru>
2020-01-29 22:18:46 +0500

    Add nurbs curve approximatioln node.

---
+++ nodes/curve/approximate_nurbs_curve.py
@@ -0,0 +1,108 @@
+
+from sverchok.utils.logging import info, exception
+
+try:
+    from geomdl import fitting
+    geomdl_available = True
+except ImportError as e:
+    info("geomdl package is not available, NURBS Curve node will not be available")
+    geomdl_available = False
+
+import bpy
+from bpy.props import FloatProperty, EnumProperty, BoolProperty, IntProperty
+
+from sverchok.node_tree import SverchCustomTreeNode, throttled
+from sverchok.data_structure import updateNode, zip_long_repeat, fullList
+
+from sverchok_extra.data.curve import SvExGeomdlCurve
+
+if geomdl_available:
+    
+    class SvExApproxNurbsCurveNode(bpy.types.Node, SverchCustomTreeNode):
+        """
+        Triggers: NURBS Curve
+        Tooltip: Interpolate NURBS Curve
+        """
+        bl_idname = 'SvExApproxNurbsCurveNode'
+        bl_label = 'Approximate NURBS Curve'
+        bl_icon = 'CURVE_NCURVE'
+
+        degree : IntProperty(
+                name = "Degree",
+                min = 2, max = 6,
+                default = 3,
+                update = updateNode)
+
+        centripetal : BoolProperty(
+                name = "Centripetal",
+                default = False,
+                update = updateNode)
+
+        @throttled
+        def update_sockets(self, context):
+            self.inputs['PointsCnt'].hide_safe = not self.has_points_cnt
+
+        has_points_cnt : BoolProperty(
+                name = "Specify points count",
+                default = False,
+                update = update_sockets)
+
+        points_cnt : IntProperty(
+                name = "Points count",
+                min = 3, default = 5,
+                update = updateNode)
+
+        def draw_buttons(self, context, layout):
+            layout.prop(self, 'centripetal', toggle=True)
+            layout.prop(self, 'has_points_cnt', toggle=True)
+
+        def sv_init(self, context):
+            self.inputs.new('SvVerticesSocket', "Vertices")
+            self.inputs.new('SvStringsSocket', "Degree").prop_name = 'degree'
+            self.inputs.new('SvStringsSocket', "PointsCnt").prop_name = 'points_cnt'
+            self.outputs.new('SvExCurveSocket', "Curve").display_shape = 'DIAMOND'
+            self.outputs.new('SvVerticesSocket', "ControlPoints")
+            self.outputs.new('SvStringsSocket', "Knots")
+            self.update_sockets(context)
+
+        def process(self):
+            if not any(socket.is_linked for socket in self.outputs):
+                return
+
+            vertices_s = self.inputs['Vertices'].sv_get()
+            degree_s = self.inputs['Degree'].sv_get()
+            points_cnt_s = self.inputs['PointsCnt'].sv_get()
+
+            curves_out = []
+            points_out = []
+            knots_out = []
+            for vertices, degree, points_cnt in zip_long_repeat(vertices_s, degree_s, points_cnt_s):
+                if isinstance(degree, (tuple, list)):
+                    degree = degree[0]
+                if isinstance(points_cnt, (tuple, list)):
+                    points_cnt = points_cnt[0]
+
+                kwargs = dict(centripetal = self.centripetal)
+                if self.has_points_cnt:
+                    kwargs['ctrlpts_size'] = points_cnt
+
+                curve = fitting.approximate_curve(vertices, degree, **kwargs)
+
+                points_out.append(curve.ctrlpts)
+                knots_out.append(curve.knotvector)
+
+                curve = SvExGeomdlCurve(curve)
+                curves_out.append(curve)
+
+            self.outputs['Curve'].sv_set(curves_out)
+            self.outputs['ControlPoints'].sv_set(points_out)
+            self.outputs['Knots'].sv_set(knots_out)
+
+def register():
+    if geomdl_available:
+        bpy.utils.register_class(SvExApproxNurbsCurveNode)
+
+def unregister():
+    if geomdl_available:
+        bpy.utils.unregister_class(SvExApproxNurbsCurveNode)
+
