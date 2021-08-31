697e2f0e33dd7e17f0c7ab4c594185e684c852ea
Ilya Portnov <portnov@bk.ru>
2021-08-31 22:03:29 +0500

    SDF boolean: "accumulate nested" mode.

--- nodes/sdf/sdf_boolean.py
+++ nodes/sdf/sdf_boolean.py
@@ -40,42 +40,88 @@ class SvExSdfBooleanNode(bpy.types.Node, SverchCustomTreeNode):
             min = 0.0,
             update = updateNode)
 
+    def update_sockets(self, context):
+        self.inputs['SDF1'].hide_safe = self.accumulate_nested
+        self.inputs['SDF2'].hide_safe = self.accumulate_nested
+        self.inputs['SDFs'].hide_safe = not self.accumulate_nested
+
+    accumulate_nested : BoolProperty(
+            name = "Accumulate Nested",
+            default = False,
+            update = update_sockets)
+
     def sv_init(self, context):
         self.inputs.new('SvScalarFieldSocket', "SDF1")
         self.inputs.new('SvScalarFieldSocket', "SDF2")
+        self.inputs.new('SvScalarFieldSocket', "SDFs")
         self.inputs.new('SvStringsSocket', "KValue").prop_name = 'k_value'
         self.outputs.new('SvScalarFieldSocket', "SDF")
+        self.update_sockets(context)
 
     def draw_buttons(self, context, layout):
         layout.prop(self, 'operation')
+        layout.prop(self, 'accumulate_nested')
+
+    def _accumulate(self, sdfs, k):
+        if self.operation == 'UNION':
+            op = union
+        elif self.operation == 'INTERSECTION':
+            op = intersection
+        else:
+            op = difference
+
+        if not sdfs:
+            return sdfs
+
+        result = sdfs[0]
+        for sdf in sdfs[1:]:
+            result = op(result, sdf, k=k)
+
+        return result
 
     def process(self):
         if not any(socket.is_linked for socket in self.outputs):
             return
 
-        sdf1_s = self.inputs['SDF1'].sv_get()
-        sdf2_s = self.inputs['SDF2'].sv_get()
         ks_s = self.inputs['KValue'].sv_get()
-
-        sdf1_s = ensure_nesting_level(sdf1_s, 2, data_types=(SvScalarField,))
-        sdf2_s = ensure_nesting_level(sdf2_s, 2, data_types=(SvScalarField,))
         ks_s = ensure_nesting_level(ks_s, 2)
 
         sdf_out = []
-        for params in zip_long_repeat(sdf1_s, sdf2_s, ks_s):
-            new_sdf = []
-            for sdf1, sdf2, k in zip_long_repeat(*params):
-                sdf1 = scalar_field_to_sdf(sdf1, 0)
-                sdf2 = scalar_field_to_sdf(sdf2, 0)
-                if self.operation == 'UNION':
-                    sdf = union(sdf1, sdf2, k=k)
-                elif self.operation == 'INTERSECTION':
-                    sdf = intersection(sdf1, sdf2, k=k)
-                else:
-                    sdf = difference(sdf1, sdf2, k=k)
-                field = SvExSdfScalarField(sdf)
-                new_sdf.append(field)
-            sdf_out.append(new_sdf)
+
+        if not self.accumulate_nested:
+            sdf1_s = self.inputs['SDF1'].sv_get()
+            sdf2_s = self.inputs['SDF2'].sv_get()
+
+            sdf1_s = ensure_nesting_level(sdf1_s, 2, data_types=(SvScalarField,))
+            sdf2_s = ensure_nesting_level(sdf2_s, 2, data_types=(SvScalarField,))
+
+            for params in zip_long_repeat(sdf1_s, sdf2_s, ks_s):
+                new_sdf = []
+                for sdf1, sdf2, k in zip_long_repeat(*params):
+                    sdf1 = scalar_field_to_sdf(sdf1, 0)
+                    sdf2 = scalar_field_to_sdf(sdf2, 0)
+                    if self.operation == 'UNION':
+                        sdf = union(sdf1, sdf2, k=k)
+                    elif self.operation == 'INTERSECTION':
+                        sdf = intersection(sdf1, sdf2, k=k)
+                    else:
+                        sdf = difference(sdf1, sdf2, k=k)
+                    field = SvExSdfScalarField(sdf)
+                    new_sdf.append(field)
+                sdf_out.append(new_sdf)
+
+        else:
+            sdfs_s = self.inputs['SDFs'].sv_get()
+            sdfs_s = ensure_nesting_level(sdfs_s, 3, data_types=(SvScalarField,))
+
+            for params in zip_long_repeat(sdfs_s, ks_s):
+                new_sdf = []
+                for sdfs, k in zip_long_repeat(*params):
+                    sdfs = [scalar_field_to_sdf(f, 0) for f in sdfs]
+                    sdf = self._accumulate(sdfs, k)
+                    field = SvExSdfScalarField(sdf)
+                    new_sdf.append(field)
+                sdf_out.append(new_sdf)
 
         self.outputs['SDF'].sv_set(sdf_out)
 
