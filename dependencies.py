843496e5649f99b197a942778d8a4b4735f9ca94
Ilya Portnov <portnov@bk.ru>
2022-12-13 11:10:48 +0500

    Add pyexcel dependencies.

--- dependencies.py
+++ dependencies.py
@@ -36,3 +36,31 @@ try:
 except ImportError:
     sdf = None
 
+pyexcel_d = ex_dependencies["pyexcel"] = SvDependency("pyexcel", "https://github.com/pyexcel/pyexcel")
+try:
+    import pyexcel
+    pyexcel_d.module = pyexcel
+except ImportError:
+    pyexcel = None
+
+pyexcel_xls_d = ex_dependencies["pyexcel_xls"] = SvDependency("pyexcel_xls", "https://github.com/pyexcel/pyexcel-xls")
+try:
+    import pyexcel_xls
+    pyexcel_xls_d.module = pyexcel_xls
+except ImportError:
+    pyexcel_xls = None
+
+pyexcel_xlsx_d = ex_dependencies["pyexcel_xlsx"] = SvDependency("pyexcel_xlsx", "https://github.com/pyexcel/pyexcel-xlsx")
+try:
+    import pyexcel_xlsx
+    pyexcel_xlsx_d.module = pyexcel_xlsx
+except ImportError:
+    pyexcel_xlsx = None
+
+pyexcel_ods_d = ex_dependencies["pyexcel_ods"] = SvDependency("pyexcel_ods", "https://github.com/pyexcel/pyexcel-ods")
+try:
+    import pyexcel_ods
+    pyexcel_ods_d.module = pyexcel_ods
+except ImportError:
+    pyexcel_ods = None
+
