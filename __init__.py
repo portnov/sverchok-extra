71c4221e20043cd4def9774e52b4a4da6c578f09
satabol <satabol@yandex.ru>
2024-03-08 19:50:24 +0300

    - move menu elements into one root group menu - append icons into group menus

--- __init__.py
+++ __init__.py
@@ -26,172 +26,64 @@ if __name__ != "sverchok_extra":
 
 from sverchok_extra import icons
 from sverchok_extra import settings
+from sverchok_extra.nodes_index import nodes_index
 from sverchok_extra.utils import show_welcome
 
 logger = logging.getLogger('sverchok.extra')
 
+# Convert struct to menu.
+# In:
+#   - source struct
+#   - lambda to convert tuples (final items are tuples in format ("{path}utils.o3d_import", "{class_name}SvO3ImportNode"))
+def convert_config(obj, func=None):
+    if not func:
+        func = lambda elem: elem # call only on tuples
+    cls_names = []
+    if type(obj)==dict:
+        cls_names = dict()
 
-def nodes_index():
-    return [("Extra Surfaces", [
-                ("surface.smooth_spline", "SvExBivariateSplineNode"),
-                ("surface.curvature_lines", "SvExSurfaceCurvatureLinesNode"),
-                ("surface.implicit_surface_solver", "SvExImplSurfaceSolverNode"),
-                ("surface.triangular_mesh", "SvExGalGenerateMeshNode")
-            ]),
-            ("Extra Curves", [
-                ("curve.intersect_surface_plane", "SvExCrossSurfacePlaneNode"),
-                ("curve.fourier_curve", "SvFourierCurveNode"),
-                ("curve.approximate_fourier_curve", "SvApproxFourierCurveNode"),
-                ("curve.interpolate_fourier_curve", "SvInterpFourierCurveNode"),
-                ("curve.geodesic_curve", "SvExGeodesicCurveNode"),
-                None,
-                ("curve.nurbs_goal_point", "SvNurbsCurvePointsGoalNode"),
-                ("curve.nurbs_goal_tangent", "SvNurbsCurveTangentsGoalNode"),
-                ("curve.nurbs_goal_closed", "SvNurbsCurveClosedGoalNode"),
-                ("curve.nurbs_goal_cpt", "SvNurbsCurveCptGoalNode"),
-                ("curve.nurbs_solver", "SvNurbsCurveSolverNode")
-            ]),
-            ("Extra Fields", [
-                ("field.vfield_lines_on_surface", "SvExVFieldLinesOnSurfNode"),
-                ('sdf.estimate_bounds', "SvExSdfEstimateBoundsNode")
-            ]),
-            ("Extra Solids", [
-                ("solid.solid_waffle", "SvSolidWaffleNode")
-            ]),
-            ("Extra Spatial", [
-                ("spatial.delaunay3d_surface", "SvDelaunayOnSurfaceNode"),
-                ("spatial.delaunay_mesh", "SvDelaunayOnMeshNode")
-            ]),
-            ("Extra Matrix", [
-                ('matrix.project_matrix', "SvProjectMatrixNode"),
-            ]),
-            ('SDF Primitives', [
-                ("sdf_primitives.sdf_sphere", "SvExSdfSphereNode"),
-                ("sdf_primitives.sdf_box", "SvExSdfBoxNode"),
-                ("sdf_primitives.sdf_platonic_solid", "SvExSdfPlatonicSolidNode"),
-                ("sdf_primitives.sdf_plane", "SvExSdfPlaneNode"),
-                ("sdf_primitives.sdf_slab", "SvExSdfSlabNode"),
-                ("sdf_primitives.sdf_rounded_box", "SvExSdfRoundedBoxNode"),
-                ("sdf_primitives.sdf_torus", "SvExSdfTorusNode"),
-                ("sdf_primitives.sdf_cylinder", "SvExSdfCylinderNode"),
-                ("sdf_primitives.sdf_rounded_cylinder", "SvExSdfRoundedCylinderNode"),
-                ("sdf_primitives.sdf_capsule", "SvExSdfCapsuleNode"),
-                None,
-                ("sdf_primitives.sdf2d_circle", "SvExSdf2dCircleNode"),
-                ("sdf_primitives.sdf2d_hexagon", "SvExSdf2dHexagonNode"),
-                ("sdf_primitives.sdf2d_polygon", "SvExSdf2dPolygonNode"),
-            ]),
-            ('SDF Operations', [
-                ('sdf.sdf_translate', 'SvExSdfTranslateNode'),
-                ('sdf.sdf_scale', 'SvExSdfScaleNode'),
-                ('sdf.sdf_rotate', 'SvExSdfRotateNode'),
-                ('sdf.sdf_orient', 'SvExSdfOrientNode'),
-                ('sdf.sdf_transform', 'SvExSdfTransformNode'),
-                None,
-                ('sdf.sdf_boolean', 'SvExSdfBooleanNode'),
-                ('sdf.sdf_blend', 'SvExSdfBlendNode'),
-                ('sdf.sdf_transition_linear', 'SvExSdfLinearTransitionNode'),
-                ('sdf.sdf_transition_radial', 'SvExSdfRadialTransitionNode'),
-                ('sdf.sdf_dilate_erode', 'SvExSdfDilateErodeNode'),
-                ('sdf.sdf_shell', 'SvExSdfShellNode'),
-                ('sdf.sdf_twist', 'SvExSdfTwistNode'),
-                ('sdf.sdf_linear_bend', 'SvExSdfLinearBendNode'),
-                None,
-                ('sdf.sdf_slice', 'SvExSdfSliceNode'),
-                ('sdf.sdf_extrude', 'SvExSdfExtrudeNode'),
-                ('sdf.sdf_extrude_to', 'SvExSdfExtrudeToNode'),
-                ('sdf.sdf_revolve', 'SvExSdfRevolveNode'),
-                None,
-                ('sdf.sdf_generate', 'SvExSdfGenerateNode'),
-            ]),
-            ("Data", [
-                ("data.spreadsheet", "SvSpreadsheetNode"),
-                ("data.data_item", "SvDataItemNode"),
-                ("data.excel_read", "SvReadExcelNode"),
-                ("data.excel_write", "SvWriteExcelNode")
-            ]),
-            ("API", [
-                ('exchange.api_in', 'SvExApiInNode'),
-                ('exchange.api_out', 'SvExApiOutNode'),
-            ]),
-            ('Array', [
-                ('array.input_value', 'SvInputValueNode'),
-                ('array.input_array', 'SvInputArrayNode'),
-                ('array.arr_number_range', 'SvArrNumberRangeNode'),
-                ('array.random_array', 'SvRandomArrayNode'),
-                ('array.py_to_array', 'SvPyToArrayNode'),
-                ('array.arr_vector_in', 'SvArrVectorInNode'),
-                ('array.arr_matrix_in', 'SvArrMatrixInNode'),
-                ('array.arr_polyline', 'SvArrPolylineNode'),
-                None,
-                ('array.arr_math', 'SvArrMathNode'),
-                ('array.bridge_polylines', 'SvArrBridgePolylinesNode'),
-                ('array.arr_polyline_length', 'SvArrPolylineLengthNode'),
-                ('array.arr_resample_polyline', 'SvResamplePolylineNode'),
-                ('array.arr_subdivide_polyline', 'SvArrSubdividePolylineNode'),
-                ('array.arr_vector_math', 'SvArrVectorMathNode'),
-                ('array.arr_rotate_vector', 'SvArrRotateVectorNode'),
-                ('array.move_vertices', 'SvArrMoveVerticesNode'),
-                ('array.arr_vector_noise', 'SvArrVectorNoiseNode'),
-                ('array.matrix_transform', 'SvArrMatrixTransformNode'),
-                ('array.move_mesh_array', 'SvArrMoveMeshNode'),
-                ('array.join_mesh_array', 'SvArrJoinMeshNode'),
-                ('array.flip_mesh_normals_array', 'SvArrFlipMeshNormalsNode'),
-                None,
-                ('array.array_length', 'SvArrayLengthNode'),
-                ('array.run_lengths', 'SvArrRunLengthsNode'),
-                ('array.zip_array', 'SvZipArrayNode'),
-                ('array.set_field', 'SvSetFieldNode'),
-                ('array.unzip_array', 'SvUnzipArrayNode'),
-                ('array.arr_get_item', 'SvArrGetItemNode'),
-                ('array.slice_array', 'SvSliceArrayNode'),
-                ('array.concatenate_arrays', 'SvConcatenateArraysNode'),
-                ('array.to_regular_array', 'SvToRegularArrayNode'),
-                ('array.new_axis', 'SvNewAxisNode'),
-                ('array.unflatten_array', 'SvUnflattenArrayNode'),
-                ('array.unflattening_array', 'SvUnflatteningArrayNode'),
-                ('array.flatten_array', 'SvFlattenArrayNode'),
-                ('array.flattening_array', 'SvFlatteningArrayNode'),
-                ('array.broadcast_arrays', 'SvBroadcastArraysNode'),
-                ('array.where_array', 'SvWhereArrayNode'),
-                ('array.local_index', 'SvLocalIndexNode'),
-                None,
-                ('array.array_to_py', 'SvArrayToPyNode'),
-                ('array.print_array', 'SvPrintArrayNode'),
-                ('array.array_viewer', 'SvArrMeshViewerNode'),
-            ])
-    ]
+    for elem in obj:
+        if elem==None: # this is menu items break
+            cls_names.append( func(elem) )
+        elif type(elem)==tuple:
+            cls_names.append( func(elem) ) # this is menu item - tuple of two params
+        elif type(obj)==dict:
+            res = convert_config(obj[elem], func) # this is submenu
+            if res:
+                cls_names[elem]=res
+        elif type(obj)==list:
+            res = convert_config(elem, func)
+            if res:
+                cls_names.append(res)
+        else:
+            raise Exception("Menu struct error")
+    return cls_names
 
-
-def convert_config(config):
-    new_form = []
-    for cat_name, items in config:
-        new_items = []
-        for item in items:
-            if item is None:
-                new_items.append('---')
-                continue
-            path, bl_idname = item
-            new_items.append(bl_idname)
-        cat = {cat_name: new_items}
-        new_form.append(cat)
-    return new_form
-
-
-add_node_menu.append_from_config(convert_config(nodes_index()))
-
+# function as argument for convert_config. call only on tuples
+def collect_classes_names(elem):
+    if elem is None:
+        res = '---'  # menu splitter. Used by Sverchok.
+    elif isinstance(elem[0], dict): # property of menugroup, ex: ({'icon_name': 'MESH_BOX'}) for icon.
+        res = elem[0]
+    else:
+        res = elem[1]  # class name to bind to menu Shift-A
+    return res
+nodes_items = convert_config(nodes_index(), collect_classes_names)
+add_node_menu.append_from_config( nodes_items )
 
 def make_node_list():
     modules = []
     base_name = "sverchok_extra.nodes"
-    index = nodes_index()
-    for category, items in index:
-        for item in items:
-            if not item:
-                continue
-            module_name, node_name = item
-            module = importlib.import_module(f".{module_name}", base_name)
-            modules.append(module)
+    arr_items = []
+    def collect_module_names(elem):
+        if elem is not None:
+            if isinstance(elem[0], str):
+                arr_items.append(elem[0])
+        return elem
+    convert_config(nodes_index(), collect_module_names)
+    for module_name in arr_items:
+        module = importlib.import_module(f".{module_name}", base_name)
+        modules.append(module)
     return modules
 
 imported_modules = [icons] + make_node_list()
