994449e4fe7f0011534ed911eba14965edb57016
Ilya V. Portnov <portnov@bk.ru>
2025-08-03 23:56:27 +0500

    "Appproximate Fourier Curve" node update.

--- nodes/curve/approximate_fourier_curve.py
+++ nodes/curve/approximate_fourier_curve.py
@@ -6,6 +6,7 @@
 # License-Filename: LICENSE
 
 import numpy as np
+from math import pi
 
 import bpy
 from bpy.props import FloatProperty, EnumProperty, BoolProperty, IntProperty
@@ -28,8 +29,14 @@ class SvApproxFourierCurveNode(SverchCustomTreeNode, bpy.types.Node):
 
     degree : IntProperty(
             name = "Degree",
-            min = 1, max = 6,
+            min = 1,
             default = 3,
+            update = updateNode)
+
+    omega : FloatProperty(
+            name = "Omega",
+            min = 0.0,
+            default = pi,
             update = updateNode)
 
     metric: EnumProperty(name='Metric',
@@ -37,15 +44,29 @@ class SvApproxFourierCurveNode(SverchCustomTreeNode, bpy.types.Node):
         default="DISTANCE", items=supported_metrics + xyz_metrics,
         update=updateNode)
 
+    def update_sockets(self, context):
+        self.inputs['Omega'].hide_safe = self.algorithm != 'LSTSQ'
+        updateNode(self, context)
+
+    algorithm : EnumProperty(name = "Algorithm",
+        items = [
+            ('FIT', "Curve fitting", "Curve fitting", 0),
+            ('LSTSQ', "Least squares", "Least squares", 1)
+        ],
+        update = update_sockets)
+
     def draw_buttons(self, context, layout):
         layout.prop(self, 'metric')
+        layout.prop(self, 'algorithm')
 
     def sv_init(self, context):
         self.inputs.new('SvVerticesSocket', "Vertices")
         self.inputs.new('SvStringsSocket', "Degree").prop_name = 'degree'
+        self.inputs.new('SvStringsSocket', "Omega").prop_name = 'omega'
         self.outputs.new('SvCurveSocket', "Curve")
         self.outputs.new('SvStringsSocket', "Omega")
         self.outputs.new('SvVerticesSocket', "Amplitudes")
+        self.update_sockets(context)
 
     def process(self):
         if not any(socket.is_linked for socket in self.outputs):
@@ -53,9 +74,11 @@ class SvApproxFourierCurveNode(SverchCustomTreeNode, bpy.types.Node):
 
         vertices_s = self.inputs['Vertices'].sv_get()
         degree_s = self.inputs['Degree'].sv_get()
+        omega_s = self.inputs['Omega'].sv_get()
         input_level = get_data_nesting_level(vertices_s)
         vertices_s = ensure_nesting_level(vertices_s, 4)
         degree_s = ensure_nesting_level(degree_s, 2)
+        omega_s = ensure_nesting_level(omega_s, 2)
 
         nested_output = input_level > 3
 
@@ -63,12 +86,15 @@ class SvApproxFourierCurveNode(SverchCustomTreeNode, bpy.types.Node):
         points_out = []
         omega_out = []
 
-        for params in zip_long_repeat(vertices_s, degree_s):
+        for params in zip_long_repeat(vertices_s, degree_s, omega_s):
             new_curves = []
             new_points = []
             new_omega = []
-            for vertices, degree in zip_long_repeat(*params):
-                curve = SvFourierCurve.approximate(np.array(vertices), degree, metric=self.metric)
+            for vertices, degree, omega in zip_long_repeat(*params):
+                if self.algorithm == 'FIT':
+                    curve = SvFourierCurve.approximate_fit(np.array(vertices), degree, metric=self.metric)
+                else:
+                    curve = SvFourierCurve.approximate_lstsq(np.array(vertices), omega, degree, metric=self.metric)
                 amplitudes = [tuple(curve.start)] + curve.coeffs.tolist()
                 omega = curve.omega
 
